f92073cf3323d88434a293b7d4e0ed6ff1688aac
igor rocha <igortice@gmail.com>
2014-12-20 19:56:22 -0300

    modeis ok e dados vindo do banco

---
+++ module/Contato/src/Contato/Model/ContatoTable.php
@@ -0,0 +1,51 @@
+<?php
+
+// namespace de localizacao do nosso model
+namespace Contato\Model;
+
+// import Zend\Db
+use //Zend\Db\Adapter\Adapter,
+    //Zend\Db\ResultSet\ResultSet,
+    Zend\Db\TableGateway\TableGateway;
+
+class ContatoTable
+{
+    protected $tableGateway;
+
+    /**
+     * 
+     * @param TableGateway $tableGateway
+     */
+    public function __construct(TableGateway $tableGateway) {
+        $this->tableGateway = $tableGateway;
+    }
+
+    /**
+     * Recuperar todos os elementos da tabela contatos
+     * 
+     * @return ResultSet
+     */
+    public function fetchAll()
+    {
+        return $this->tableGateway->select();
+    }
+
+    /**
+     * Localizar linha especifico pelo id da tabela contatos
+     * 
+     * @param type $id
+     * @return ModelContato
+     * @throws Exception
+     */
+    public function find($id)
+    {
+        $id  = (int) $id;
+        $rowset = $this->tableGateway->select(array('id' => $id));
+        $row = $rowset->current();
+        if (!$row) {
+            throw new Exception("Não foi encontrado contado de id = {$id}");
+        }
+
+        return $row;
+    }
+}
